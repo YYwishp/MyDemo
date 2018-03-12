96aa561eab7cb677e3750208e2e367070221da1e
yywishp <yywishp@gmail.com>
2018-03-12 22:08:16 +0800

    bugly测试

--- bugly_test/src/main/java/com/gyx/bugly_test/MainActivity.java
+++ bugly_test/src/main/java/com/gyx/bugly_test/MainActivity.java
@@ -10,20 +10,31 @@ public class MainActivity extends AppCompatActivity {
 
 
 	private TextView text;
-
+	private TextView text2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		text = (TextView) findViewById(R.id.text);
-//		text.setOnClickListener(new View.OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				Toast.makeText(MainActivity.this, "bug已被修复", Toast.LENGTH_SHORT).show();
-//			}
-//		});
+		text.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "bug已被修复", Toast.LENGTH_SHORT).show();
+			}
+		});
 		text.setText("bugfixed");
+		
+	
+		
+		text2 = (TextView) findViewById(R.id.text2);
 
+		text2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+
+				Toast.makeText(MainActivity.this, "啦啦啦啦", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 }
