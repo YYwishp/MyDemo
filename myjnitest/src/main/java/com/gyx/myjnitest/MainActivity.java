9e99f64aabfb155a570da66754d59361d7101b5b
YYwishp <yywishp@gmail.com>
2018-03-12 14:15:41 +0800

    提交jni编译代码

--- myjnitest/src/main/java/com/gyx/myjnitest/MainActivity.java
+++ myjnitest/src/main/java/com/gyx/myjnitest/MainActivity.java
@@ -2,6 +2,7 @@ package com.gyx.myjnitest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,12 +12,18 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-
-
 		text = (TextView) findViewById(R.id.text);
-
 		text.setText(JniTest.getPackname());
+		int a = 100;
+		int b = 200;
+		swap(a, b);
+		Log.e("test--", a + "----" + b);
+	}
 
+	public void swap(int x, int y) {
+		int temp;
+		temp = x;
+		x = y;
+		y = temp;
 	}
 }
